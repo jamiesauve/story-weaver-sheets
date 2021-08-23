b6f14af2781fccd97185428430a11169a543cb33
Jamie Sauve <jamiesauve@zohomail.com>
2021-08-23 12:45:00 -0500

    Add a ScrollableList component

---
+++ src/components/ui/ScrollableList.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+const This = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  overflow: scroll;
+`
+
+const ListItem = styled.div`
+  margin: .3rem 0;
+`
+
+const ScrollableList = (props) => {
+  console.log('children', props.children)
+  return (
+    <This>
+      {props.children.map((item, index) => (
+        <ListItem
+          key={index}
+        >
+          {item}
+        </ListItem>
+      ))}
+    </This>
+  )
+}
+
+export default ScrollableList;
