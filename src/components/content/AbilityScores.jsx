9e92faa84914d68ab90c2eeb5fbae01ac880f8b8
Jamie Sauve <jamiesauve@zohomail.com>
2021-08-23 20:19:20 -0500

    Add Ability Score big numbers

--- src/components/content/AbilityScores.jsx
+++ src/components/content/AbilityScores.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 import ScrollableList from '../ui/ScrollableList'
 
-import { abilityScoresAsLabels } from '../../constants/abilityScores';
+import { abilityScores } from '../../constants/abilityScores';
 import RollableListItem from '../ui/RollableListItem';
 
 const This = styled.div`
@@ -12,14 +12,50 @@ const This = styled.div`
   justify-content: center;
 `
 
+const Row = styled.div`
+  display: flex;
+`
+
+const AbilityScoreBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-right: .6rem;
+`
+
+const AbilityScoreValue = styled.div`
+  font-size: 3rem;
+  font-weight: bold;
+
+`
+
+const AbilityScoreLabel = styled.div`
+  margin-top: -1rem;
+  font-size: 1rem;
+`
+
 const AbilityScores = () => {
-  const listItems = abilityScoresAsLabels.map((abilityScoreLabel, index) => (
-    <RollableListItem 
-      bonus={index - 3}
-      command="GO CRAZY"
-      label={abilityScoreLabel}
-      key={abilityScoreLabel}
-    />
+  const listItems = abilityScores.map((abilityScore, index) => (
+    <Row
+      key={abilityScore.label}
+    >
+      <AbilityScoreBox>
+        <AbilityScoreValue>
+          {10}
+        </AbilityScoreValue>
+
+        <AbilityScoreLabel>
+          {abilityScore.abbr}
+        </AbilityScoreLabel>
+      </AbilityScoreBox>
+
+      <RollableListItem 
+        bonus={index - 3}
+        command="GO CRAZY"
+        label={abilityScore.label}
+        />
+      </Row>
   ))
 
   return (
